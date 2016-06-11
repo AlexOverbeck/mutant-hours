7f3217422ddb8b104f815bf214cbad694ffd3e0c
Seth Baughman <sbaughman1@me.com>
2016-06-11 11:08:54 -0400

    creates Mutant constructor and binds form values to new Mutant object with ng-model.  change addMutant function to use Mutant object instead of hard-coded value

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -8,11 +8,20 @@
   function MutantListController() {
     var vm = this;
 
+    function Mutant() {
+      this.name = '';
+      this.phone = '';
+      this.topic = '';
+      this.complete = false;
+      this.notified = false;
+    }
+
     vm.mutants = ['cyclops', 'rogue', 'wolverine'];
     vm.addMutant = addMutant;
+    vm.newMutant = new Mutant();
 
     function addMutant() {
-      vm.mutants.push('professor x');
+      vm.mutants.push(vm.newMutant);
     }
   }
 
