15d22033601e9b23ac48615d2626aa06162e5d23
Seth Baughman <sbaughman1@me.com>
2016-06-12 17:45:08 -0400

    adds sendText property and function to text message service

--- app/core/textMessage.service.js
+++ app/core/textMessage.service.js
@@ -6,8 +6,23 @@
     .factory('textMessageService', textMessageService);
 
   function textMessageService() {
-    var service = {};
+    var service = {
+      sendText: sendText
+    };
 
     return service;
+
+    /////////////
+
+    function sendText(mutant) {
+      var newText = {
+        name: mutant.name,
+        phoneNumber: mutant.phone,
+        topic: mutant.topic
+      };
+      firebaseDataService.texts.push(newText);
+      mutant.notified = true;
+      vm.mutants.$save(mutant);
+    }
   }
 })();
