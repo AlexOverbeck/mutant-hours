29d586bac599303808ce72caeb0b2676c918a369
Seth Baughman <sbaughman1@me.com>
2016-06-13 14:54:02 -0400

    removes ng-controller directive from navbar.html.  adds NavbarController to xtNavbar.

--- app/layout/navbar.directive.js
+++ app/layout/navbar.directive.js
@@ -8,7 +8,23 @@
   function xtNavbar() {
     return {
       templateUrl: 'app/layout/navbar.html',
-      restrict: 'E'
+      restrict: 'E',
+      controller: NavbarController,
+      controllerAs: 'vm'
     };
+  }
+
+  NavbarController.$inject = ['$state', 'authService'];
+
+  function NavbarController($state, authService) {
+    var vm = this;
+
+    vm.isLoggedIn = authService.isLoggedIn;
+    vm.logout = logout;
+
+    function logout() {
+      authService.logout();
+      $state.go('home');
+    }
   }
 })();
