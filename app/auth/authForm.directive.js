59e21f491ca118cd56abca65c828aadf0ee3086e
Seth Baughman <sbaughman1@me.com>
2016-06-15 17:43:09 -0400

    creates authForm directive and controller, includes script tag on index.html.  adds blank authForm.html page

---
+++ app/auth/authForm.directive.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.auth')
+    .directive('xtAuthForm', xtAuthForm);
+
+  function xtAuthForm() {
+    return {
+      templateUrl: 'app/auth/authForm.html',
+      restrict: 'E',
+      controller: AuthFormController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        
+      }
+    };
+  }
+
+  function AuthFormController() {
+    var vm = this;
+  }
+
+})();
